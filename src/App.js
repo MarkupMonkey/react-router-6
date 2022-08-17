d39c5e1057ed676eaaa287d458c0be1af7936723
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-17 20:58:38 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Products from './pages/Products';
 import Error from './pages/Error'
 
 function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='products' element={<Products />} />
-      <Route path='*' element={<Error />} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <nav>our navbar</nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
