e057d07d1afa6e1c675d2f365ce60e3fbc023063
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-17 21:38:39 +0200

    Index Pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,12 @@
-import { Link, Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+
 
 const Home = () => {
   return (
-    <>
-      <Navbar />
 
-      <section className='section'>
-        <h2>Home Page</h2>
-        <Outlet />
-      </section>
-    </>
+    <section className='section'>
+      <h2>Home Page</h2>
+    </section>
+
   );
 };
 export default Home;
